cb978e4a7d5f1a5e351c058e0f07c3373f3af409
root <root@jessie.raw>
2017-03-23 14:27:28 +0000

    esse trabalho é hard

--- trabalho05.sh
+++ trabalho05.sh
@@ -7,7 +7,6 @@ while (($NUM != 0)); do
 	echo "Digite um salário"
 	read SAL
 	TEMP=$(echo "scale=2; $SAL > $MENOR || $SAL > $MEDIO || $SAL > $MENOR" | bc)
-	echo "$TEMP"
 	if (($TEMP == 1)); then
 		TEMP_S=$SAL
 	fi
@@ -18,10 +17,6 @@ TEMP=$(echo "scale=2; $TEMP_S > $MAIOR" | bc)
 	else
 		TEMP_MAIOR=$TEMP_S
 	fi 
-
-
-
-
 TEMP=$(echo "scale=2; $TEMP_MAIOR > $MEDIO && $TEMP_MAIOR < $MAIOR" | bc)
 	if (($TEMP == 1)); then
 		TEMP_MEDIO=$MEDIO
@@ -29,8 +24,6 @@ TEMP=$(echo "scale=2; $TEMP_MAIOR > $MEDIO && $TEMP_MAIOR < $MAIOR" | bc)
 	else
 		TEMP_MEDIO=$TEMP_MAIOR
 	fi
-
-
 TEMP=$(echo "scale=2; $TEMP_MEDIO > $MENOR && $TEMP_MEDIO < $MEDIO" | bc)
 	if (($TEMP == 1)); then
 		MENOR=$TEMP_MENOR
@@ -42,17 +35,14 @@ TEMP=$(echo "scale=2; $SAL > 0.00" | bc)
 		NUM=1000
 	fi
 done
-echo "$MENOR , $MEDIO, $MAIOR"
 TEMP=$(echo "scale=2; $MENOR > 0.00" | bc)
 if (($TEMP == 1)); then
 	echo "Esse eh o menor numero: " $MENOR
 fi
-
 TEMP=$(echo "scale=2; $MEDIO > 0.00" | bc)
 if (($TEMP == 1)); then
 	echo "Esse eh o medio: " $MEDIO
 fi
-
 TEMP=$(echo "scale=2; $MAIOR > 0.00" | bc)	
 if (($TEMP == 1)); then
 	echo "Esse eh o maior numero: " $MAIOR
