8b23cf36d64e8796bcac1bc6041dfb22c4f11bec
root <root@jessie.raw>
2017-03-23 14:47:13 +0000

    Arrumei

--- trabalho05.sh
+++ trabalho05.sh
@@ -6,9 +6,10 @@ NUM=1
 while (($NUM != 0)); do
 	echo "Digite um salário"
 	read SAL
-	TEMP=$(echo "scale=2; $SAL > $MENOR || $SAL > $MEDIO || $SAL > $MENOR" | bc)
+	TEMP=$(echo "scale=2; $SAL > $MENOR || $SAL > $MEDIO || $SAL > $MAIOR" | bc)
 	if (($TEMP == 1)); then
 		TEMP_S=$SAL
+
 	fi
 TEMP=$(echo "scale=2; $TEMP_S > $MAIOR" | bc)
 	if (($TEMP == 1)); then
@@ -26,7 +27,7 @@ TEMP=$(echo "scale=2; $TEMP_MAIOR > $MEDIO && $TEMP_MAIOR < $MAIOR" | bc)
 	fi
 TEMP=$(echo "scale=2; $TEMP_MEDIO > $MENOR && $TEMP_MEDIO < $MEDIO" | bc)
 	if (($TEMP == 1)); then
-		MENOR=$TEMP_MENOR
+		MENOR=$TEMP_MEDIO
 	fi
 TEMP=$(echo "scale=2; $SAL > 0.00" | bc) 
 	if (($TEMP == 0)); then
@@ -37,14 +38,14 @@ TEMP=$(echo "scale=2; $SAL > 0.00" | bc)
 done
 TEMP=$(echo "scale=2; $MENOR > 0.00" | bc)
 if (($TEMP == 1)); then
-	echo "Esse eh o menor numero: " $MENOR
+	echo "Esse eh o menor numero: $MENOR"
 fi
 TEMP=$(echo "scale=2; $MEDIO > 0.00" | bc)
 if (($TEMP == 1)); then
-	echo "Esse eh o medio: " $MEDIO
+	echo "Esse eh o medio: $MEDIO"
 fi
 TEMP=$(echo "scale=2; $MAIOR > 0.00" | bc)	
 if (($TEMP == 1)); then
-	echo "Esse eh o maior numero: " $MAIOR
+	echo "Esse eh o maior numero: $MAIOR"
 fi
 
