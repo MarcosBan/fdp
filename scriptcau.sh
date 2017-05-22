4aad6fc95bf804eb8866f5fcad22ad2c39daf39d
root <root@debian>
2017-05-22 12:52:16 -0300

    Ta indo

--- scriptcau.sh
+++ scriptcau.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+################################FUNÇÕES SECUNDÁRIAS#############################
 
+#-------------------------------Modificar Grupos-------------------------------#
+ADUG(){
+
+}
+
+RMUG(){
+
+}
+
+CRG(){
+}
 ################################FUNÇÕES PRIMARIAS###############################
 CAU(){
 VZS=1
@@ -12,25 +24,26 @@ OP=$( dialog	--stdout			\
 	2 'Apagar usuário' 			\
 	3 'Voltar')
 if (( $OP == "1")); then
-CRIAR=$( dialog 	--stdout				\
-			--title 'Nome do usuário'		\
-			--inputbox 'Insira o nome do pivet'	\
-			0 0)
+	CRIAR=$( dialog 	--stdout				\
+				--title 'Nome do usuário'		\
+				--inputbox 'Insira o nome do pivet'	\
+				0 0)
 
-PASS=$( dialog 		--stdout				\
-			--title 'Nome do usuário' 		\
-			--inputbox 'INSIRA A SENHA' 		\
-			0 0)
+	PASS=$( dialog 		--stdout				\
+				--title 'Nome do usuário' 		\
+				--inputbox 'INSIRA A SENHA' 		\
+				0 0)
 
 useradd -m -d /home/$CRIAR -r -s /bin/bash $CRIAR
 (echo $PASS ; echo $PASS) | passwd $CRIAR
 
 
+
 elif (( $OP == "2")); then
-APAGAR=$( dialog 	--stdout		\
-			--title 'Nome do usuário' \
-			--inputbox 'Insira o nome do pivet' \
-			0 0)
+	APAGAR=$( dialog 	--stdout		\
+				--title 'Nome do usuário' \
+				--inputbox 'Insira o nome do pivet' \
+				0 0)
 
 userdel -r $APAGAR
 
@@ -56,6 +69,8 @@ INICIAR
 }
 
 
+
+#----------------------------------------------------------------------------#
 CAG(){
 VZS=1
 while (($VZS != 0)); do 
@@ -68,10 +83,10 @@ OP=$( dialog	--stdout			\
 	3 'Voltar')
 
 if (( $OP == "1")); then
-CRIAR=$( dialog 	--stdout				\
-			--title 'Nome do usuário' 		\
-			--inputbox 'Insira o nome do grupo'	\
-			0 0)
+	CRIAR=$( dialog 	--stdout				\
+				--title 'Nome do usuário' 		\
+				--inputbox 'Insira o nome do grupo'	\
+				0 0)
 
 groupadd $CRIAR
 
@@ -99,6 +114,8 @@ done
 
 INICIAR
 }
+
+#----------------------------------------------------------------------------#
 MODU(){
 
 VZS=1
@@ -119,15 +136,21 @@ dialog	--yes-label Confirma --no-label Voltar				\
 
 CONF=$?
 	if [$CONF == 0]; then
-		PERM=$( dialog --stdout					\
-				--title	'Escolha uma classe'		\
-				--menu 'Qual deseja aplicar no usuário' \
-				0 0 0					\					1 'Criar ou apagar usuario' 	\
-				2 'Mudar senha do  usuário'	\
-				3 'Criar ou apagar grupos'	\
-				4 'Modificar usuarios'		\
-				5 'Modificar grupos')
-				
+		
+	PASS=$( dialog 	--stdout					\
+				--title 'Pesquisa' 			\
+				--inputbox 'Insira o nome do usuário' 	\
+				0 0)
+
+	CONF_P=$( dialog 	--stdout					\
+				--title 'Confirmação' 				\
+				--passwordbox 'Confirme a senha do usuário' 	\
+				0 0)
+		(echo $CONF_P ; echo $CONF_P) | passwd $PESQUISA
+	else
+
+		dialog --stdout --msgbox "Senha inconpativél" 0 0
+		
 	fi
 
 
@@ -146,24 +169,29 @@ done
 INICIAR
 }
 
+
+#----------------------------------------------------------------------------#
 MODG(){
 
 VZS=1
 while (($VZS != 0)); do 
 
-NOME_U=$(dialog 	--stdout				\
-			--title 'Nome do usuário' 		\
-			--inputbox 'Insira o nome do usuário'	\
-			0 0)
+		OPI=$( dialog    --stdout                        \
+                --title 'Opções para usuário'   \
+                --menu 'Escolha uma opção'      \
+        0 0 0                           	\
+        1 'Adcionar usuario a um grupo'			\
+        2 'Remover usuario de um grupo'			\
+        3 'Criar ou remover grupo'			\
+        4 'Voltar)
 
-NOME_G=$(dialog 	--stdout				\
-			--title 'Nome do grupo' 		\
-			--inputbox 'Insira o nome do grupo' 	\
-			0 0)
-
-	gpasswd -a $NOME_U $NOME_G
-
-
+case $OPI in
+        1)ADUG ;;
+        2)RMUG ;;
+        3)CRG ;;
+        #4)SAIR ;;
+        *)echo 'Opção incorreta';;
+esac
 if (($? == 0)); then
 
  dialog --stdout --infobox 'Realizado com sucesso' 0 0
@@ -179,6 +207,16 @@ done
 
 INICIAR
 }
+#----------------------------------------------------------------------------#
+MODP(){
+
+
+
+}
+
+
+
+#----------------------------------------------------------------------------#
 
 INICIAR(){
 
@@ -191,20 +229,18 @@ OP=$( dialog	--stdout			\
 	3 'Criar ou apagar grupos'	\
 	4 'Modificar usuarios'		\
 	5 'Modificar grupos'		\
-	6 'Sair')
+	6 'Modificar permissões'	\
+	7 'Sair')
 
 case $OP in
 	1)CAU ;;
 	2)MODU ;;
 	3)CAG ;;
-	4)MODU ;;
-	5)MODG ;;
+	4)MODG ;;
+	5)MODP ;;
 	#6)SAIR ;;
 	*)echo 'Opção incorreta';;
 esac
-
-echo $CAU
-
 
 }
 ##################################PROGRAMA####################################
